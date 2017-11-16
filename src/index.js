4a72c72272b3a7b45f9eb40581a02f17947a331d
Will Scripps <WLScripps@gmail.com>
2017-11-16 14:03:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import changeCount from './reducers/changeCount';
+import createStore from './createStore'
+
+const store = createStore(changeCount)
+// console.log(store.getState(), "store.getState() from index.js");
+// console.log(store, "store from index.js");
+
+
 
 export function render() {
   ReactDOM.render(
-    <App />,
+    <App store={store}/>,
     document.getElementById('root')
   );
 };
 
-render();
+store.dispatch({type: "@@INIT"})
+
+//render()
